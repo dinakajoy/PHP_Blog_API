41bd920e4ade1f5710b985cbd7c26ffd2d212708
odinaka-joy <odinakajoy909@gmail.com>
2020-01-26 22:34:52 +0100

    create the blog home page

--- frontend/assets/js/index.js
+++ frontend/assets/js/index.js
@@ -1,20 +1,33 @@
 const loadPosts = document.querySelector('#loadPosts');
 const loadCategories = document.querySelector('#loadCategories');
 
-// const loadPosts = () => {
-
-// }
+const createPost = (post) => {
+    let div = document.createElement('div');
+    div.className = 'post';
+    let a = document.createElement('a');
+    a.setAttribute('href', `./postDetail.html?id=${post.id}`);
+    let h3 = document.createElement('h3');
+    h3.textContent = post.title;
+    let p = document.createElement('p');
+    if(post.body.length <= 40) {
+        p.textContent = `${post.body}`;
+    }
+    p.textContent = `${post.body.slice(0, 40)}`;
+    let small = document.createElement('small');
+    small.textContent = 'read more...';
+    small.style.color = '#888';
+    p.appendChild(small);
+    div.appendChild(a);
+    a.appendChild(h3);
+    a.appendChild(p);
+    loadPosts.appendChild(div);
+}
 
 const getPosts = async () => {
     const response = await fetch('http://localhost:5000/api/post/getPosts.php');
     const posts = await response.json();
     posts.forEach(post => {
-        let div = document.createElement('div');
-        div.className = 'post';
-        let h3 = document.createElement('h3');
-        h3.textContent = post.title;
-        div.appendChild(h3);
-        loadPosts.appendChild(div);
+        createPost(post);
     })
 }
 
